5c4d75cbfae4341e6555d4d4b87a2fe6c56dfa72
hana-bananaa <debartosiaki@gmail.com>
2023-06-25 04:26:28 -0300

    ajustes desktop lawer de nova coleção

--- layout/src/components/NewColection/NewColection.js
+++ layout/src/components/NewColection/NewColection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NewColectionIMG from "../../assets/novacolecao.jpg"
+import NewColectionIMG from "../../assets/novacolecao.png"
 
 import "./styles.css";
 
@@ -7,8 +7,8 @@ const NewColection = () => {
   const features = [
     {
       img: NewColectionIMG,
-      title: 'Produtos importados',
-      description: 'Produto de Alta Qualidade',
+      title: 'Lorem ipsum' ,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum mauris posuere fusce proin mattis. Hendrerit cras ut nunc enim dictum. Mattis proin ut quis donec sed eget nulla. Vel mi ut mauris integer. Nibh sagittis in lobortis sed cursus condimentum velit pharetra. Amet luctus ut vulputate scelerisque placerat consequat. Neque arcu mi iaculis id. Vel vitae, pharetra, a nec tristique. Feugiat id tortor eu mauris pulvinar velit massa. Ut ornare augue eget convallis volutpat aliquet. Sed sed pellentesque porttitor phasellus donec condimentum sit sapien.',
     }
   ];
 
